428ed3e152e83484b34c3ffe94fd982525ad1311
David Reina <david.reina@mail.escuelaing.edu.co>
2017-03-21 15:33:28 -0500

    Parte 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,18 +1,31 @@
 var stompClient = null;
+var x=0;
+var y=0;
+
 
 function connect() {
     var socket = new SockJS('/stompendpoint');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        
-        stompClient.subscribe('/topic/TOPICOXX', function (data) {
-           
-            
+
+        stompClient.subscribe('/topic/newpoint', function (data) {
+            //var x = data.x;
+            //var y = data.y;
+            var theObject=JSON.parse(data.body);
+            callBack(theObject);
+
+
         });
     });
 }
+function sendPoint() {
 
+    stompClient.send("/topic/newpoint", {}, JSON.stringify({x: 10, y: 10}));
+}
+function callBack(theObject){
+    alert("Posx"+theObject["x"]+"Posy"+theObject["y"]);
+}
 function disconnect() {
     if (stompClient != null) {
         stompClient.disconnect();
